31cf79f6dbbc51ac26ba04b445ff3eba73c40039
kenylopezUMG <klopezm58@miumg.edu.gt>
2024-05-11 10:19:30 -0600

    se corrigio el login para que sea mas limpio

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 @section('content')
 <div class="container">
